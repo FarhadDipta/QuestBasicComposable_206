8326b0f7eabe1ab809ea86c807aad60608d9070e
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2025-10-16 15:00:54 +0700

    menambahkan fungsi TataletakRow

--- app/src/main/java/com/example/farhaddipta/Tataletak.kt
+++ app/src/main/java/com/example/farhaddipta/Tataletak.kt
@@ -34,5 +34,18 @@ fun TataletakColumn(modifier: Modifier) {
     }
 }
 
+@Composable
+fun TataletakRow(modifier: Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
+
 class Tataletak {
 }
