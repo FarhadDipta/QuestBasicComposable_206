79bb30396220b4373d34f8ca2ea3c836d7cebc75
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2025-10-16 15:08:15 +0700

    Menambahkan library ke file MainActivity

--- app/src/main/java/com/example/farhaddipta/MainActivity.kt
+++ app/src/main/java/com/example/farhaddipta/MainActivity.kt
@@ -1,9 +1,8 @@
-package com.example.farhaddipta
+package com.example.mylayout
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +10,5 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.farhaddipta.ui.theme.FarhadDiptaTheme
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            FarhadDiptaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FarhadDiptaTheme {
-        Greeting("Android")
-    }
-}
+import androidx.core.view.WindowCompat.enableEdgeToEdge
+import com.example.mylayout.ui.theme.MyLayoutThemeMyLayoutThemea
